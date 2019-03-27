d930d4f986d811c6aee605de0b80dbd25ac9e012
fabian <fabian.heredia@gmail.com>
2019-03-27 11:08:03 -0500

    modificaciones para agarrar variables por url

--- js/mainmap.js
+++ js/mainmap.js
@@ -1,15 +1,15 @@
 idmap = "d469bc9456124f7f9985ac97204fb9a4"
 
-var  getQueryVariable = ()=>
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       console.log("lo que trae la variable "+query)
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               //if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+var getQueryVariable = () => {
+  console.log(window.location.search)
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  console.log("lo que trae la variable " + query)
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    //if(pair[0] == variable){return pair[1];}
+  }
+  return (false);
 }
 
 require([
@@ -17,7 +17,7 @@ require([
   "esri/views/MapView",
   "esri/widgets/Bookmarks",
   "esri/widgets/Home"
-], function(WebMap, MapView, Bookmarks, Home) {
+], function (WebMap, MapView, Bookmarks, Home) {
   let baseExtent = {
     xmin: -2588034.141,
     ymin: -1467944.146,
@@ -25,7 +25,7 @@ require([
     ymax: 2690230.193,
     spatialReference: 3857
   };
-getQueryVariable();
+  getQueryVariable();
   const webmap = new WebMap({
     portalItem: {
       // autocasts as new PortalItem()
@@ -54,7 +54,7 @@ getQueryVariable();
   });
   view.ui.add(homeBtn, "top-left");
 
-  view.on("drag", function(event) {
+  view.on("drag", function (event) {
     console.log(view.extent);
     // prevents zooming with the mouse-wheel event
     if (view.extent.xmin < baseExtent.xmin) event.stopPropagation();
@@ -66,7 +66,7 @@ getQueryVariable();
   });
 
   // bonus - how many bookmarks in the webmap?
-  webmap.when(function() {
+  webmap.when(function () {
     if (webmap.bookmarks && webmap.bookmarks.length) {
       console.log("Bookmarks: ", webmap.bookmarks.length);
     } else {
